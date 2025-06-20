93e73bb48a19051a1d3d0cabdcebd1f79b80ab6c
hellohack262 <hellohack214@gmail.com>
2025-06-20 18:04:34 +0530

    Update MainActivity.java

--- android/app/src/main/java/com/example/flutter_recce_app/MainActivity.java
+++ android/app/src/main/java/com/example/flutter_recce_app/MainActivity.java
@@ -1,7 +1,6 @@
-
 package com.example.flutter_recce_app;
 
-import io.flutter.embedding.android.FlutterActivity;
+import io.flutter.embedding.android.FlutterActivity; // ✅ NEW
 
 public class MainActivity extends FlutterActivity {
 }
